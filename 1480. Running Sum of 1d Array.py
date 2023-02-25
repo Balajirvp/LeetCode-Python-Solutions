f91a24b951ec23cbee7b872199fe6a2a7aa8a97a
Balaji Padmanathan <104417912+Balajirvp@users.noreply.github.com>
2023-02-25 14:28:40 +0000

    Added new solutions

--- 1480. Running Sum of 1d Array.py
+++ 1480. Running Sum of 1d Array.py
@@ -17,3 +17,9 @@ class Solution:
             else:
                 arr.append(nums[i]+arr[-1])
         return arr
+   
+# Solution 3 (54 ms, 14 MB)
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        return [sum(nums[:i+1]) for i in range(len(nums))]
